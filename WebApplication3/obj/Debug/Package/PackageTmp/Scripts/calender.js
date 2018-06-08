710fe1134fd7a50623515f479f03d0f5d9577703
kannkris <kannkris@DIN77003059>
2018-06-08 17:58:00 +0530

    calender upadted

---
+++ WebApplication3/obj/Debug/Package/PackageTmp/Scripts/calender.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $("#SelectedItem").change(function (event) {
+        var month = $('#SelectedItem :selected').text();
+        var date = $("#SelectedItem").val();
+        var valueArray = [];
+        valueArray.push(month);
+        valueArray.push(date);      
+        $.ajax({
+            url: "Home/Index",
+            data: { values: valueArray },
+            cache: false,
+            type: "POST",
+            dataType: "html",
+
+            success: function (data, textStatus, XMLHttpRequest) {
+               
+                // $("#divPartialView").html( data );
+
+                var select = $("#SelectedDate");
+                select.empty();
+               
+            }
+        });
+    });
+});
